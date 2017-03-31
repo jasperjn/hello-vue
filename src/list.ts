c3cf6cc7c4daab03960cb19076bafcda681269d4
jasperjn <jaspjnn@gmail.com>
2017-03-31 10:28:34 +0800

    accomplish list

--- src/list.ts
+++ src/list.ts
@@ -1,5 +1,9 @@
 import Vue, { ComponentOptions } from 'vue';
 
+interface Example1 extends Vue {
+    items: { message: string }[];
+}
+
 var example1 = new Vue({
     el: '#example-1',
     data: {
@@ -8,7 +12,7 @@ var example1 = new Vue({
             { message: 'Bar' }
         ]
     }
-});
+}) as Example1;
 
 var example2 = new Vue({
     el: '#example-2',
@@ -124,3 +128,83 @@ new Vue({
         }
     }
 } as ComponentOptions<TodoVM>);
+
+// =============================================================================
+// Array Change Detection
+// https://vuejs.org/v2/guide/list.html#Array-Change-Detection
+// =============================================================================
+
+// Mutation Methods
+// https://vuejs.org/v2/guide/list.html#Mutation-Methods
+example1.items.push({ message: 'Baz' });
+// items: [{message: 'Foo'}, {message: 'Bar'}, 'Barz']
+
+example1.items.push(...[{ message: 'Barz' }, { message: 'Fooz' }]);
+// items: [{message: 'Foo'}, {message: 'Bar'}, {message: 'Baz'}, '{message: Barz'}, {message: 'Fooz'}]
+
+example1.items.pop();
+// items: [{message: 'Foo'}, {message: 'Bar'}, {message: 'Baz'}, {message: 'Barz'}]
+
+example1.items.shift();
+// items: [{message: 'Bar'}, {message: 'Baz'}, {message: 'Barz'}]
+
+example1.items.unshift({ message: 'Foo' });
+// items: [{message: 'Foo'}, {message: 'Bar'}, {message: 'Baz'}, {message: 'Barz'}]
+
+example1.items.splice(0, 1);
+// items: [{message: 'Bar'}, {message: 'Baz'}, {message: 'Barz'}]
+
+example1.items.sort((a, b) => {
+    if (a.message > b.message) {
+        return 1;
+    }
+    if (a.message < b.message) {
+        return -1;
+    }
+    return 0;
+});
+
+example1.items.reverse();
+
+// Replacing An Array
+// https://vuejs.org/v2/guide/list.html#Replacing-an-Array
+example1.items = example1.items.filter(item => item.message.match(/Bar?z/));
+
+// =============================================================================
+// Caveats
+// https://vuejs.org/v2/guide/list.html#Caveats
+// =============================================================================
+
+// Docs says Vue cannot detect following changes, But it works...
+example1.items[2] = { message: 'Should not be worked' };
+example1.items[2].message = 'But it works!';
+
+// Recommended
+Vue.set(example1.items, 2, { message: 'Works' });
+example1.items.splice(2, 1, { message: 'Works also' });
+
+example1.items.splice(2);
+
+// =============================================================================
+// Displaying Filtered/Sorted Results
+// https://vuejs.org/v2/guide/list.html#Displaying-Filtered-Sorted-Results
+// =============================================================================
+
+interface Filtered extends Vue {
+    numbers: number[];
+}
+
+new Vue({
+    el: '#filtered',
+    data: { numbers: [1, 2, 3, 4, 5] },
+    computed: {
+        evenNumbers() {
+            return this.numbers.filter(number => number % 2 === 0);
+        }
+    },
+    methods: {
+        even(numbers: number[]) {
+            return numbers.filter(number => number % 2 === 0);
+        }
+    }
+} as ComponentOptions<Filtered>);
